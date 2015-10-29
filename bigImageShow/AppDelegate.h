14218bec139c7b1738691c9e0af7fc261b409752
TomBin647 <gaobin847607048@icloud.com>
2015-10-29 10:57:08 +0800

    Initial Commit

---
+++ bigImageShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bigImageShow
+//
+//  Created by 高彬 on 15/10/29.
+//  Copyright © 2015年 高彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
